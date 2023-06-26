eb7de8afb1c65e924c18185e285a8677fb322a14
FarnazLEGRAND <116880622+FarnazLEGRAND@users.noreply.github.com>
2023-06-26 13:47:57 +0200

    init

--- README.md
+++ README.md
@@ -12,7 +12,9 @@ _______________________
 
 _______________________
     ** Premier contrôleur Rest :
-
+ ---- avant tt dans le terminal:
+ php bin/consol ma:con Movie: pour cree control
+ -----par default
 1. Créer un contrôleur MovieController et lui rajouter un constructeur contenant un MovieRepository en private
 	
 2. Rajouter une route /api/movie, préciser dans la route que sa méthode est GET
@@ -20,4 +22,8 @@ _______________________
 3. Faire un return d'un $this->json() avec le findAll du repository à l'intérieur
 	
 4. Tester cette méthode sur thunder client en faisant une requête GET vers localhost:8000/api/movie
+	
+5. Rajouter une nouvelle route sur /api/movie/{id} en GET également, et dedans faire un findById, si le retour est null on fait un return d'un $this->json avec un message Resource Not Found et le status 404  , sinon on renvoie le movie
+	
+6. Rajouter une nouvelle route /api/movie/{id} mais cette fois ci en method DELETE et dedans on supprime le movie
 
---
+++ src/Controller/MovieController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\MovieRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/api/movie')]
+class MovieController extends AbstractController
+{
+
+    public function __construct(private MovieRepository $repo) {}
+
+
+    #[Route(methods: 'GET')]
+    public function all(): JsonResponse
+    {
+        return $this->json($this->repo->findAll());
+    }
+
+    #[Route('/{id}',methods: 'GET')]
+    public function one(int $id): JsonResponse
+    {
+        $movie = $this->repo->findById($id);
+        if($movie == null) {
+            return $this->json('Resource Not found', 404);
+        }
+
+        return $this->json($movie);
+    }
+
+    #[Route('/{id}',methods: 'DELETE')]
+    public function delete(int $id): JsonResponse
+    {
+        $movie = $this->repo->findById($id);
+        if($movie == null) {
+            return $this->json('Resource Not found', 404);
+        }
+        $this->repo->delete($id);
+
+        return $this->json(null, 204);
+    }
+}
+
+
+
+// class MovieController extends AbstractController
+// {
+//     public function __construct(private MovieRepository $repo){}
+//     #[Route('/api/movie', methods: 'GET')]
+
+//     public function all()
+//     {
+//         return $this->json($this->repo->findAll());
+        
+//     }
+
+//     #[Route('/{id}', methods: 'GET')]
+
+//     public function one(int $id):JsonResponse
+//     {
+//         $movie=$this->repo->findById($id);
+//         if($movie==null){
+//         return $this->json('resource not found',404);
+//     }
+//     return $this->json($movie);
+
+
+
+//     }
+// }
